e654bf13c99d359fc0616652c5735492151fd95d
LucasDev13 <lucaspontes140@gmail.com>
2023-01-02 17:21:53 -0300

    Criado o arquivo security-group para separar essa config.

--- main.tf
+++ main.tf
@@ -39,23 +39,6 @@ resource "aws_instance" "dev5" {
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
 
-resource "aws_security_group" "acesso-ssh"{
-  name        = "acesso-ssh"
-  description = "acesso-ssh"
-
-  ingress {
-    description      = "acesso-ssh"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["201.131.137.19/32"]
-  }
-
-  tags = {
-    Name = "ssh"
-  }
-}
-
 resource "aws_s3_bucket" "dev4" {
   bucket = "lucasdevlabs-dev4"
 
